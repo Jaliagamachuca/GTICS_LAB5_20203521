b25999a47e75cbe6b7da9a7eae26e6d90ffec343
Julio Cesar <a20203521@pucp.edu.pe>
2024-05-08 19:21:26 -0500

    1 creacion del proyecto

---
+++ Lab5_Julio_Aliaga/src/main/java/com/example/lab5_julio_aliaga/Lab5JulioAliagaApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab5_julio_aliaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab5JulioAliagaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab5JulioAliagaApplication.class, args);
+    }
+
+}
---
+++ Lab5_Julio_Aliaga/src/test/java/com/example/lab5_julio_aliaga/Lab5JulioAliagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab5_julio_aliaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5JulioAliagaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
